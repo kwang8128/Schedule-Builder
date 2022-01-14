f37c34bcd0cc0bde73093fb6fb83fb20628af72f
Kai Wang <kaiwang@Kais-Air.fios-router.home>
2022-01-14 18:23:47 -0500

    useless

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 @app.get("/")
 async def root():
     return {"message: Hello World"}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.post
